0f739b321537aef5223e9dc2ff3210e9520d4327
charleslukes <charleslukes28@gmail.com>
2023-07-06 07:09:10 +0100

    add router file

---
+++ users_service/router.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func userRouter() http.Handler  {
+	r := chi.NewRouter()
+
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/signin",  signIn)
+		r.Post("/signup",  signUp)
+	})
+
+	return r
+}
