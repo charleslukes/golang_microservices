b8f39ab6b5fb28c5d8fd943e975416b446a5876d
charleslukes <charleslukes28@gmail.com>
2023-07-06 07:09:02 +0100

    add user service main entry file

---
+++ users_service/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+const DefaultDatabase = "customerResource"
+var mh *MongoHandler
+
+func main() {
+	mongoDbConnection := "mongodb://localhost:27017"
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("Could not find port in the environment")
+	}
+	
+    mh = NewHandler(mongoDbConnection) 
+
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))	
+
+	// mount router
+	r.Mount("/v1", userRouter())
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":" + port,
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
