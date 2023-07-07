1c56212378687fee8413056100052c26836a4b57
charleslukes <charleslukes28@gmail.com>
2023-07-07 16:45:31 +0100

    update controller

--- product_service/controller.go
+++ product_service/controller.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
-
 	"github.com/charleslukes/golang_microservices/helper"
-	"github.com/go-chi/chi/v5"
-	validation "github.com/go-ozzo/ozzo-validation/v4"
-	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
+	"strings"
 )
 
 type payload struct {
@@ -16,25 +14,48 @@ type payload struct {
 	Data    interface{} `json:"data"`
 }
 
-func getProfile(w http.ResponseWriter, r *http.Request) {
-	emailAddress := chi.URLParam(r, "emailAddress")
-	err := validation.Validate(emailAddress, is.Email)
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := &Product{}
+
+	err := decoder.Decode(&params)
 
 	if err != nil {
-		helper.RespondWithError(w, 404, fmt.Sprintf("error: %v", err))
+		helper.RespondWithError(w, 400, fmt.Sprintf("error: %v", err))
 		return
 	}
 
-	user := &User{}
-	err = mh.GetOne(user, bson.M{"email": emailAddress})
-	if err != nil {
-		helper.RespondWithError(w, 404, fmt.Sprintf("error: %v", err))
+	// validate
+	v_err := params.Validate()
+
+	if v_err != nil {
+		helper.RespondWithError(w, 400, fmt.Sprintf("%v", v_err))
 		return
 	}
-	payload := payload{
-		Message: "successful",
-		Data:    user,
-	}
 
-	helper.RespondWithJson(w, 200, payload)
+	// check if product name already exits
+	p_err := mh.GetOne(params, bson.M{"name": params.Name})
+
+	if p_err == nil {
+		params.Name = strings.ToLower(params.Name)
+		res, err := mh.AddOne(params)
+
+		if err != nil {
+			helper.RespondWithError(w, 400, fmt.Sprintf("%v", v_err))
+			return
+		}
+
+		payload := payload{
+			Message: "product created",
+			Data:    res,
+		}
+		helper.RespondWithJson(w, 201, payload)
+	} else {
+		helper.RespondWithError(w, 400, fmt.Sprintf("Product with name:`%v` already exits", params.Name))
+	}
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	products := mh.Get(struct{}{})
+	helper.RespondWithJson(w, 201, products)
 }
