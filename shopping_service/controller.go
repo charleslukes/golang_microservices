65dd31b695d74f3927ce8d0eb2af068104126144
charleslukes <charleslukes28@gmail.com>
2023-07-14 02:41:40 +0100

    add create order method

--- shopping_service/controller.go
+++ shopping_service/controller.go
@@ -5,3 +5,35 @@ type payload struct {
 	Data    interface{} `json:"data"`
 }
 
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := &Shop{}
+
+	err := decoder.Decode(params)
+
+	if err != nil {
+		helper.RespondWithError(w, 400, err.Error())
+		return
+	}
+
+	vErr := params.CreateOrderValidate()
+
+	if vErr != nil {
+		helper.RespondWithError(w, 400, vErr.Error())
+		return
+	}
+
+	res, dbErr := mh.AddOne(params)
+
+	if dbErr != nil {
+		helper.RespondWithError(w, 404, dbErr.Error())
+		return
+	}
+
+	payload := payload{
+		Message: "order created",
+		Data:    res,
+	}
+
+	helper.RespondWithJson(w, 201, payload)
+}
